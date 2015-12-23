07f9c67275e03d4b97bc9c3eb9524001f8c10699
simeydotme <simey.me@gmail.com>
2015-12-24 00:24:06 +0800

    change options handling

--- index.js
+++ index.js
@@ -8,61 +8,38 @@ module.exports = postcss.plugin("postcss-move-media-queries", function ( opts ) 
     
         var defaults = {
 
-            combine: true,
-            filter: false
+            regex: ".*",
+            keep: false,
+            invert: false
 
         };
 
         opts = _.extend( defaults, opts ) || defaults;
+        var test = new RegExp( opts.regex , "i" );
 
-        if ( opts.filter && _.isObject( opts.filter ) ) {
+        // remove all non-media at-rules;
+        
+        if ( css.nodes.length ) {
 
-            var test = new RegExp( opts.filter.regex , opts.filter.flags );
+            // remove non-media nodes if we don't want to keep
+            if( !opts.keep ) {
 
-            // remove all non-media at-rules;
-            
-            if ( css.nodes.length ) {
+                css.walkAtRules( function( rule ) {
 
-                // remove non-media nodes if we don't want to keep
-                if( !opts.filter.keep ) {
-
-                    css.walkAtRules( function( rule ) {
-
-                        if ( rule.name !== "media" ) {
-
-                            rule.remove();
-
-                        }
-
-                    });
-
-                    // remove every rule which is not inside
-                    // the remaining media queries
-
-                    css.walkRules( function( rule ) {
-
-                        if ( rule.parent.type !== "atrule" ) {
-
-                            rule.remove();
-
-                        }
-
-                    });
-
-                }
-
-                // remove every rule which doesn't
-                // match our passed regex
-
-                css.walkAtRules( "media", function( rule ) {
-
-                    var matched = test.test( rule.params );
-
-                    if ( !matched && !opts.filter.invert ) {
+                    if ( rule.name !== "media" ) {
 
                         rule.remove();
 
-                    } else if ( matched && opts.filter.invert ) {
+                    }
+
+                });
+
+                // remove every rule which is not inside
+                // the remaining media queries
+
+                css.walkRules( function( rule ) {
+
+                    if ( rule.parent.type !== "atrule" ) {
 
                         rule.remove();
 
@@ -71,6 +48,26 @@ module.exports = postcss.plugin("postcss-move-media-queries", function ( opts ) 
                 });
 
             }
+
+            // remove every rule which doesn't
+            // match our passed regex
+
+            css.walkAtRules( "media", function( rule ) {
+
+                var matched = test.test( rule.params );
+
+                if ( !matched && !opts.invert ) {
+
+                    rule.remove();
+
+                } else if ( matched && opts.invert ) {
+
+                    rule.remove();
+
+                }
+
+            });
+
 
         }
         
